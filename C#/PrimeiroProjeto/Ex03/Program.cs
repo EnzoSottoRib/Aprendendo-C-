12fc5d061141657b63a2332fa8c2d2a9283f6ff1
Your Name <you@example.com>
2024-08-15 22:16:14 -0300

    erros ex03 resolver amanhã na aula

---
+++ C#/PrimeiroProjeto/Ex03/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+/* Receber dois números inteiros e informar qual valor lido é o menor e qual é o maior. */
+
+public class OutraOutraClasse{
+    public void MaiorEMenor(){
+        Console.WriteLine("Digite o primeiro número: ");
+    }
+}
+
+
